4349ee4987a1fa716c81c9ed93c0c0c114ac17d7
Vasil Ivanov <vasilivanov93@gmail.com>
2019-04-08 15:53:31 +0300

    Create anonymous guard.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './components/shared/home/home.component';
 import {RegisterComponent} from './components/authentication/register/register.component';
 import {LoginComponent} from './components/authentication/login/login.component';
+import {AnonymousGuard} from './core/guards/anonymous.guard';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: HomeComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'register', component: RegisterComponent, canActivate: [AnonymousGuard] },
+  { path: 'login', component: LoginComponent, canActivate: [AnonymousGuard] }
 ];
 
 @NgModule({
