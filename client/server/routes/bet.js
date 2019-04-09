223a6338bf9b9f12403844719492cad787dc5ba5
Vasil Ivanov <vasilivanov93@gmail.com>
2019-04-09 13:19:00 +0300

    List all finished predictions in history page for users.

--- client/server/routes/bet.js
+++ client/server/routes/bet.js
@@ -1,168 +1,158 @@
-const express = require('express')
-const authCheck = require('../middleware/auth-check');
-const Bet = require('../models/Bet');
+const express = require("express");
+const authCheck = require("../middleware/auth-check");
+const Bet = require("../models/Bet");
 
-const router = new express.Router()
+const router = new express.Router();
 
-function validateFurnitureForm (payload) {
-  const errors = {}
-  let isFormValid = true
-  let message = ''
+function validateFurnitureForm(payload) {
+  const errors = {};
+  let isFormValid = true;
+  let message = "";
 
-  if (!payload || typeof payload.homeTeam !== 'string' || payload.homeTeam.length < 3) {
-    isFormValid = false
-    errors.make = 'Home team must be more than 3 symbols.'
+  if (!payload || typeof payload.homeTeam !== "string" || payload.homeTeam.length < 3) {
+    isFormValid = false;
+    errors.homeTeam = "Home team must be more than 3 symbols.";
   }
 
-  if (!payload || typeof payload.awayTeam !== 'string' || payload.awayTeam.length < 3) {
-    isFormValid = false
-    errors.model = 'Away team must be more than 3 symbols.'
+  if (!payload || typeof payload.awayTeam !== "string" || payload.awayTeam.length < 3) {
+    isFormValid = false;
+    errors.awayTeam = "Away team must be more than 3 symbols.";
   }
-  
-  if (!payload || typeof payload.result !== 'string' || payload.result.length < 5) {
-    isFormValid = false
-    errors.model = 'Result must be more than 5 symbols.'
+
+  if (!payload || typeof payload.prediction !== "string" || payload.prediction.length < 2) {
+    isFormValid = false;
+    errors.prediction = "Prediction must be more than 1 symbols.";
   }
-  
-  if (!payload || typeof payload.prediction !== 'string' || payload.prediction.length < 3) {
-    isFormValid = false
-    errors.model = 'Prediction must be more than 3 symbols.'
-  }
-  
-  if (!payload || typeof payload.odd !== 'string' || payload.odd.length < 4) {
-    isFormValid = false
-    errors.model = 'Odd must be more than 4 symbols.'
-  }
-  
-    if (!payload || typeof payload.resultBet !== 'string' || payload.resultBet.length < 3) {
-    isFormValid = false
-    errors.model = 'Result bet must be more than 3 symbols.'
+
+  if (!payload || typeof payload.odd !== "string" || payload.odd.length < 1) {
+    isFormValid = false;
+    errors.odd = "Odd must be contain number.";
   }
 
   if (!isFormValid) {
-    message = 'Check the form for errors.'
+    message = "Check the form for errors.";
   }
 
   return {
     success: isFormValid,
     message,
-    errors
-  }
+    errors,
+  };
 }
 
-router.post('/create', authCheck, (req, res) => {
-  const bet = req.body
-  const validationResult = validateFurnitureForm(bet)
+router.post("/create", authCheck, (req, res) => {
+  const bet = req.body;
+  const validationResult = validateFurnitureForm(bet);
   if (!validationResult.success) {
     return res.status(400).json({
       success: false,
       message: validationResult.message,
-      errors: validationResult.errors
-    })
+      errors: validationResult.errors,
+    });
   }
 
   Bet.create(bet)
     .then(() => {
       res.status(200).json({
         success: true,
-        message: 'Bet added successfully.',
-        bet
-      })
-    })
-})
+        message: "Bet added successfully.",
+        bet,
+      });
+    });
+});
 
-router.get('/history', authCheck ,(req, res) => {
+router.get("/history", authCheck , (req, res) => {
   Bet.find()
-	.sort({date: 'descending'})
+	.sort({date: "descending"})
     .then((bets) => {
-      return res.status(200).json({message: 'Loading predictions history.', bets});
-    })
-})
+      return res.status(200).json(bets);
+    });
+});
 
-router.get('/home', authCheck ,(req, res) => {
+router.get("/home", authCheck , (req, res) => {
   Bet.find({isFinished: true})
-            .sort({date: 'descending'})
+            .sort({date: "descending"})
             .limit(5)
 			.then((betsHome) => {
-			  return res.status(200).json({message: 'Loading predictions history.', betsHome});
-			})
-})
+			  return res.status(200).json(betsHome);
+			});
+});
 
-router.delete('/delete/:id', authCheck, (req, res) => {
-  const id = req.params.id
+router.delete("/delete/:id", authCheck, (req, res) => {
+  const id = req.params.id;
 
   Bet.findById(id)
     .then((bet) => {
       if (!bet) {
         return res.status(200).json({
           success: false,
-          message: 'Bet does not exists!'
-        })
+          message: "Bet does not exists!",
+        });
       }
 
       if (!req.user.roles.includes("Admin")) {
          return res.status(401).json({
            success: false,
-           message: 'Unauthorized!'
-         })
+           message: "Unauthorized!",
+         });
       }
 
       Bet.findByIdAndDelete(id)
         .then(() => {
           return res.status(200).json({
             success: true,
-            message: 'Bet deleted successfully!'
-          })
-        })
-    })
-})
+            message: "Bet deleted successfully!",
+          });
+        });
+    });
+});
 
-router.put('/edit/:id', authCheck, (req, res) => {
+router.put("/edit/:id", authCheck, (req, res) => {
   const id = req.params.id;
   const bet = req.body;
 
   if (!bet) {
     return res.status(404).json({
       success: false,
-      message: 'Bet does not exists!'
-    })
+      message: "Bet does not exists!",
+    });
   }
 
-  if (!req.user.roles.includes('Admin')) {
+  if (!req.user.roles.includes("Admin")) {
     return res.status(401).json({
       success: false,
-      message: 'Unauthorized!'
-    })
+      message: "Unauthorized!",
+    });
   }
 
-  const validationResult = validateFurnitureForm(bet)
+  const validationResult = validateFurnitureForm(bet);
   if (!validationResult.success) {
     return res.status(400).json({
       success: false,
       message: validationResult.message,
-      errors: validationResult.errors
-    })
+      errors: validationResult.errors,
+    });
   }
 
   Bet.findByIdAndUpdate(id, bet)
     .then(() => {
       return res.status(200).json({
         success: true,
-        message: 'Bet edited successfully!'
-      })
-  })
-})
+        message: "Bet edited successfully!",
+      });
+  });
+});
 
-router.get('/:id', authCheck, (req, res) => {
-  const id = req.params.id
+router.get("/:id", authCheck, (req, res) => {
+  const id = req.params.id;
 
   Bet.findById(id)
     .then(bet => {
       if (!bet) {
         return res.status(404).json({
           success: false,
-          message: 'Entry does not exists!'
-        })
+          message: "Entry does not exists!",
+        });
       }
 
       let response = {
@@ -172,11 +162,11 @@ router.get('/:id', authCheck, (req, res) => {
         result: bet.result,
         prediction: bet.prediction,
         odd: bet.odd,
-        resultBet: bet.resultBet
-      }
+        resultBet: bet.resultBet,
+      };
 
-      res.status(200).json(response)
-    })
-})
+      res.status(200).json(response);
+    });
+});
 
-module.exports = router
+module.exports = router;
