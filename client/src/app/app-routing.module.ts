793143fd02e4337d2d1fb6d732964fcd3745ab83
Vasil Ivanov <vasilivanov93@gmail.com>
2019-04-09 13:27:16 +0300

    Create auth guard.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './components/shared/home/home.component';
 import {AnonymousGuard} from './core/guards/anonymous.guard';
+import {AuthGuard} from './core/guards/auth.guard';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: HomeComponent },
   { path: 'auth', loadChildren: './components/authentication/authentication.module#AuthenticationModule', canLoad: [AnonymousGuard]},
-  { path: 'bet', loadChildren: './components/bet/bet.module#BetModule'}
+  { path: 'bet', loadChildren: './components/bet/bet.module#BetModule', canLoad: [AuthGuard] }
 ];
 
 @NgModule({
---
+++ client/src/app/core/guards/auth.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { CanLoad, Route, Router, UrlSegment } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanLoad {
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  canLoad(route: Route, segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
+    if (this.authService.isAuthenticated()) {
+      return true;
+    }
+
+    this.router.navigate(['/auth/login']);
+
+    return false;
+  }
+}
