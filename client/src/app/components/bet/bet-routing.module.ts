72a00decfa7e61c5760638fe8dd339ea2b94311d
Vasil Ivanov <vasilivanov93@gmail.com>
2019-04-10 10:10:07 +0300

    Create admin guard fot protection routing for edit and delete bet.

--- client/src/app/components/bet/bet-routing.module.ts
+++ client/src/app/components/bet/bet-routing.module.ts
@@ -7,13 +7,14 @@ import {HistoryBetComponent} from './history-bet/history-bet.component';
 import {EditBetComponent} from './edit-bet/edit-bet.component';
 import {SingleBetResolver} from '../../core/resolvers/single-bet.resolver';
 import {DeleteBetComponent} from './delete-bet/delete-bet.component';
+import {AdminGuard} from '../../core/guards/admin.guard';
 
 const betRoutes: Route[] = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'create', component: CreateBetComponent },
   { path: 'history', component: HistoryBetComponent },
-  { path: 'edit/:id', component: EditBetComponent, resolve: { singleBet: SingleBetResolver } },
-  { path: 'delete/:id', component: DeleteBetComponent, resolve: { singleBet: SingleBetResolver } }
+  { path: 'edit/:id', component: EditBetComponent, resolve: { singleBet: SingleBetResolver }, canActivate: [AdminGuard] },
+  { path: 'delete/:id', component: DeleteBetComponent, resolve: { singleBet: SingleBetResolver }, canActivate: [AdminGuard] }
 ];
 
 @NgModule({
